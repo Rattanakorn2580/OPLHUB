e3a0fed7a5030f8fcb780adbd32d417411aed3ef
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-04 20:31:54 +0700

    Update Find.lua

--- Find.lua
+++ Find.lua
@@ -1,5 +1,5 @@
-for i,v in pairs(game:GetService("Workspace").Compass:GetChildren()) do
-  if v.Name == "TouchInterest" then
+for i,v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+  if v.Name == "Compass" then
 game.Players.LocalPlayer.Charecter.HumanoidRootPart.CFrame = v.Parent.CFrame
     wait()
   end
